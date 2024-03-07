6d50f594902c91dd87dfba8a8421b7c54634ff5d
Mads Hougesen <mads@mhouge.dk>
2024-03-07 16:55:22 +0100

    refactor: remove permission

--- src/formatters/mod.rs
+++ src/formatters/mod.rs
@@ -1,7 +1,5 @@
 use std::{
-    fs::Permissions,
     io::Write,
-    os::unix::fs::PermissionsExt,
     process::{Command, Stdio},
 };
 
@@ -27,7 +25,6 @@ pub fn setup_snippet(code: &str, file_ext: &str) -> std::io::Result<NamedTempFil
     let mut f = tempfile::Builder::new()
         .rand_bytes(12)
         .suffix(file_ext)
-        .permissions(Permissions::from_mode(0o777))
         .tempfile()?;
 
     f.write_all(code.as_bytes())?;
