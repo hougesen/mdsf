c99e9db84ed5e66fcccb98faf8d70c3047a8970c
Mads Hougesen <mads@mhouge.dk>
2024-03-07 13:05:31 +0100

    feat: implement support for biome (#7)

--- src/languages/mod.rs
+++ src/languages/mod.rs
@@ -1,20 +1,40 @@
 pub enum Language {
+    JavaScript,
+    Json,
     Lua,
     Nim,
     Python,
     Rust,
+    TypeScript,
     Zig,
 }
 
 impl Language {
     pub fn from_str(input: &str) -> Option<Self> {
         match input {
+            "js" | "jsx" | "javascript" => Some(Self::JavaScript),
+            "json" => Some(Self::Json),
             "lua" => Some(Self::Lua),
             "nim" => Some(Self::Nim),
             "python" => Some(Self::Python),
             "rust" => Some(Self::Rust),
+            "ts" | "tsx" | "typescript" => Some(Self::TypeScript),
             "zig" => Some(Self::Zig),
+
             _ => None,
+        }
+    }
+
+    pub fn to_file_ext(&self) -> &'static str {
+        match self {
+            Self::JavaScript => ".js",
+            Self::Json => ".jsonc",
+            Self::Lua => ".lua",
+            Self::Nim => ".nim",
+            Self::Python => ".py",
+            Self::Rust => ".rs",
+            Self::TypeScript => ".ts",
+            Self::Zig => ".zig",
         }
     }
 }
