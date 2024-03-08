5f1ef4d239fce72914520b35f9892bf37f7aa7f4
Mads Hougesen <mads@mhouge.dk>
2024-03-08 18:06:21 +0100

    feat: set config schema location

--- src/config.rs
+++ src/config.rs
@@ -5,8 +5,11 @@ use crate::languages::{
     typescript::TypeScript, zig::Zig,
 };
 
-#[derive(Debug, Default, serde::Serialize, serde::Deserialize, JsonSchema)]
+#[derive(Debug, serde::Serialize, serde::Deserialize, JsonSchema)]
 pub struct MdsfConfig {
+    #[schemars(skip)]
+    #[serde(rename = "$schema", default = "default_schema_location")]
+    schema: String,
     #[serde(default)]
     pub javascript: JavaScript,
     #[serde(default)]
@@ -27,6 +30,24 @@ pub struct MdsfConfig {
     pub zig: Zig,
 }
 
+impl Default for MdsfConfig {
+    #[inline]
+    fn default() -> Self {
+        Self {
+            schema: default_schema_location(),
+            javascript: JavaScript::default(),
+            json: Json::default(),
+            lua: Lua::default(),
+            nim: Nim::default(),
+            python: Python::default(),
+            rust: Rust::default(),
+            toml: Toml::default(),
+            typescript: TypeScript::default(),
+            zig: Zig::default(),
+        }
+    }
+}
+
 pub fn init_config_command() -> std::io::Result<()> {
     let current_dir = std::env::current_dir()?;
 
@@ -42,4 +63,13 @@ pub fn init_config_command() -> std::io::Result<()> {
 #[inline]
 pub fn default_enabled() -> bool {
     true
+}
+
+#[inline]
+fn default_schema_location() -> String {
+    let package_version = env!("CARGO_PKG_VERSION");
+
+    format!(
+        "https://raw.githubusercontent.com/hougesen/mdsf/main/schemas/v{package_version}/mdsf.schema.json"
+    )
 }
