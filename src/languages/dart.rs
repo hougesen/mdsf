27baba20ae8775496224139eaf6d3b2957976045
Mads Hougesen <mads@mhouge.dk>
2024-03-11 19:24:05 +0100

    test: dart

--- src/languages/dart.rs
+++ src/languages/dart.rs
@@ -44,19 +44,17 @@ impl LanguageFormatter for Dart {
 
 #[cfg(test)]
 mod test {
-    use crate::{
-        formatters::setup_snippet,
-        languages::{Language, LanguageFormatter},
-    };
+    use crate::{formatters::setup_snippet, languages::LanguageFormatter};
 
     use super::{Dart, DartFormatter};
 
-    const INPUT: &str = "";
+    const INPUT: &str = "class Adder {   int add(int a, int b) {     return a + b;   } }    ";
+
+    const EXTENSION: &str = crate::languages::Language::Dart.to_file_ext();
 
     #[test]
     fn it_should_be_enabled_by_default() {
-        let snippet =
-            setup_snippet(INPUT, Language::Dart.to_file_ext()).expect("it to save the file");
+        let snippet = setup_snippet(INPUT, EXTENSION).expect("it to save the file");
         let snippet_path = snippet.path();
 
         Dart::default()
@@ -67,8 +65,7 @@ mod test {
 
     #[test]
     fn it_should_not_format_when_enabled_is_false() {
-        let snippet =
-            setup_snippet(INPUT, Language::Dart.to_file_ext()).expect("it to save the file");
+        let snippet = setup_snippet(INPUT, EXTENSION).expect("it to save the file");
         let snippet_path = snippet.path();
 
         assert!(Dart {
@@ -78,5 +75,30 @@ mod test {
         .format(snippet_path)
         .expect("it to not fail")
         .is_none());
+    }
+
+    #[test]
+    fn test_dart_format() {
+        let l = Dart {
+            enabled: true,
+            formatter: DartFormatter::DartFormat,
+        };
+
+        let snippet = setup_snippet(INPUT, EXTENSION).expect("it to save the file");
+        let snippet_path = snippet.path();
+
+        let output = l
+            .format(snippet_path)
+            .expect("it to not fail")
+            .expect("it to be a snippet");
+
+        let expected_output = "class Adder {
+  int add(int a, int b) {
+    return a + b;
+  }
+}
+";
+
+        assert_eq!(output, expected_output);
     }
 }
