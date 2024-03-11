de47a538596e438c25625c7edbf22238196b6f2b
Mads Hougesen <mads@mhouge.dk>
2024-03-11 19:18:46 +0100

    test: validate cpp

--- src/languages/cpp.rs
+++ src/languages/cpp.rs
@@ -44,19 +44,17 @@ impl LanguageFormatter for Cpp {
 
 #[cfg(test)]
 mod test {
-    use crate::{
-        formatters::setup_snippet,
-        languages::{Language, LanguageFormatter},
-    };
+    use crate::{formatters::setup_snippet, languages::LanguageFormatter};
 
     use super::{Cpp, CppFormatter};
 
     const INPUT: &str = "";
 
+    const EXTENSION: &str = crate::languages::Language::Cpp.to_file_ext();
+
     #[test]
     fn it_should_be_enabled_by_default() {
-        let snippet =
-            setup_snippet(INPUT, Language::Cpp.to_file_ext()).expect("it to save the file");
+        let snippet = setup_snippet(INPUT, EXTENSION).expect("it to save the file");
         let snippet_path = snippet.path();
 
         Cpp::default()
@@ -67,8 +65,7 @@ mod test {
 
     #[test]
     fn it_should_not_format_when_enabled_is_false() {
-        let snippet =
-            setup_snippet(INPUT, Language::Cpp.to_file_ext()).expect("it to save the file");
+        let snippet = setup_snippet(INPUT, EXTENSION).expect("it to save the file");
         let snippet_path = snippet.path();
 
         assert!(Cpp {
@@ -78,5 +75,28 @@ mod test {
         .format(snippet_path)
         .expect("it to not fail")
         .is_none());
+    }
+
+    #[test]
+    fn test_clang_format() {
+        let l = Cpp {
+            enabled: true,
+            formatter: CppFormatter::ClangFormat,
+        };
+
+        let snippet = setup_snippet(INPUT, EXTENSION).expect("it to save the file");
+        let snippet_path = snippet.path();
+
+        let output = l
+            .format(snippet_path)
+            .expect("it to not fail")
+            .expect("it to be a snippet");
+
+        let expected_output = "int add(int a, int b) {
+  a - b;
+  return a + b;
+}";
+
+        assert_eq!(output, expected_output);
     }
 }
