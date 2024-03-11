c95c8721542d3dbf7ab2385f7aa34ff4e12184dc
Mads Hougesen <mads@mhouge.dk>
2024-03-11 19:28:17 +0100

    test: gleam

--- src/languages/gleam.rs
+++ src/languages/gleam.rs
@@ -44,19 +44,17 @@ impl LanguageFormatter for Gleam {
 
 #[cfg(test)]
 mod test {
-    use crate::{
-        formatters::setup_snippet,
-        languages::{Language, LanguageFormatter},
-    };
+    use crate::{formatters::setup_snippet, languages::LanguageFormatter};
 
     use super::{Gleam, GleamFormatter};
 
-    const INPUT: &str = "";
+    const INPUT: &str = "pub fn add(a:Int,b:Int)->Int{a+b}";
+
+    const EXTENSION: &str = crate::languages::Language::Gleam.to_file_ext();
 
     #[test]
     fn it_should_be_enabled_by_default() {
-        let snippet =
-            setup_snippet(INPUT, Language::Gleam.to_file_ext()).expect("it to save the file");
+        let snippet = setup_snippet(INPUT, EXTENSION).expect("it to save the file");
         let snippet_path = snippet.path();
 
         Gleam::default()
@@ -67,8 +65,7 @@ mod test {
 
     #[test]
     fn it_should_not_format_when_enabled_is_false() {
-        let snippet =
-            setup_snippet(INPUT, Language::Gleam.to_file_ext()).expect("it to save the file");
+        let snippet = setup_snippet(INPUT, EXTENSION).expect("it to save the file");
         let snippet_path = snippet.path();
 
         assert!(Gleam {
@@ -78,5 +75,28 @@ mod test {
         .format(snippet_path)
         .expect("it to not fail")
         .is_none());
+    }
+
+    #[test]
+    fn test_gleam_format() {
+        let l = Gleam {
+            enabled: true,
+            formatter: GleamFormatter::GleamFormat,
+        };
+
+        let snippet = setup_snippet(INPUT, EXTENSION).expect("it to save the file");
+        let snippet_path = snippet.path();
+
+        let output = l
+            .format(snippet_path)
+            .expect("it to not fail")
+            .expect("it to be a snippet");
+
+        let expected_output = "pub fn add(a: Int, b: Int) -> Int {
+  a + b
+}
+";
+
+        assert_eq!(output, expected_output);
     }
 }
