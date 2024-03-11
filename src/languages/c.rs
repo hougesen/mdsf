451e88dd1eb72c6b40fc2f10d31f8fbcabae2894
Mads Hougesen <mads@mhouge.dk>
2024-03-11 19:18:41 +0100

    test: validate c

--- src/languages/c.rs
+++ src/languages/c.rs
@@ -44,18 +44,20 @@ impl LanguageFormatter for C {
 
 #[cfg(test)]
 mod test {
-    use crate::{
-        formatters::setup_snippet,
-        languages::{Language, LanguageFormatter},
-    };
+    use crate::{formatters::setup_snippet, languages::LanguageFormatter};
 
     use super::{CFormatter, C};
 
-    const INPUT: &str = "";
+    const INPUT: &str = "int add(int a,int b){
+                a-b;
+       return a + b;
+    }";
+
+    const EXTENSION: &str = crate::languages::Language::C.to_file_ext();
 
     #[test]
     fn it_should_be_enabled_by_default() {
-        let snippet = setup_snippet(INPUT, Language::C.to_file_ext()).expect("it to save the file");
+        let snippet = setup_snippet(INPUT, EXTENSION).expect("it to save the file");
         let snippet_path = snippet.path();
 
         C::default()
@@ -66,7 +68,7 @@ mod test {
 
     #[test]
     fn it_should_not_format_when_enabled_is_false() {
-        let snippet = setup_snippet(INPUT, Language::C.to_file_ext()).expect("it to save the file");
+        let snippet = setup_snippet(INPUT, EXTENSION).expect("it to save the file");
         let snippet_path = snippet.path();
 
         assert!(C {
@@ -76,5 +78,28 @@ mod test {
         .format(snippet_path)
         .expect("it to not fail")
         .is_none());
+    }
+
+    #[test]
+    fn test_clang_format() {
+        let l = C {
+            enabled: true,
+            formatter: CFormatter::ClangFormat,
+        };
+
+        let snippet = setup_snippet(INPUT, EXTENSION).expect("it to save the file");
+        let snippet_path = snippet.path();
+
+        let output = l
+            .format(snippet_path)
+            .expect("it to not fail")
+            .expect("it to be a snippet");
+
+        let expected_output = "int add(int a, int b) {
+  a - b;
+  return a + b;
+}";
+
+        assert_eq!(output, expected_output);
     }
 }
