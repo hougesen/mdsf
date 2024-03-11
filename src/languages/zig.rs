986682a34f100dd7a62119d2c9b305c7b03a0332
Mads Hougesen <mads@mhouge.dk>
2024-03-11 18:26:22 +0100

    test: validate zig

--- src/languages/zig.rs
+++ src/languages/zig.rs
@@ -41,3 +41,58 @@ impl LanguageFormatter for Zig {
         }
     }
 }
+
+#[cfg(test)]
+mod test_zig {
+    use crate::{
+        formatters::setup_snippet,
+        languages::{Language, LanguageFormatter},
+    };
+
+    use super::{Zig, ZigFormatter};
+
+    const INPUT: &str = "
+    fn     add   (a : i32    , b :   i32 )             i32 {
+        return a + b ;
+
+    }
+    ";
+
+    const EXPECTED_OUTPUT: &str = "fn add(a: i32, b: i32) i32 {
+    return a + b;
+}
+";
+
+    #[test]
+    fn it_should_not_format_when_enabled_is_false() {
+        let l = Zig {
+            enabled: false,
+            formatter: ZigFormatter::ZigFmt,
+        };
+
+        let snippet =
+            setup_snippet(INPUT, Language::Zig.to_file_ext()).expect("it to save the file");
+        let snippet_path = snippet.path();
+
+        assert!(l.format(snippet_path).expect("it to not fail").is_none());
+    }
+
+    #[test]
+    fn zig_fmt() {
+        let l = Zig {
+            enabled: true,
+            formatter: ZigFormatter::ZigFmt,
+        };
+
+        let snippet =
+            setup_snippet(INPUT, Language::Zig.to_file_ext()).expect("it to save the file");
+        let snippet_path = snippet.path();
+
+        let output = l
+            .format(snippet_path)
+            .expect("it to not fail")
+            .expect("it to be a snippet");
+
+        assert_eq!(output, EXPECTED_OUTPUT);
+    }
+}
