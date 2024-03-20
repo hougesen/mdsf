a252f4b5bfedcc3276da8c31b040abd0650b1e76
Mads Hougesen <mads@mhouge.dk>
2024-03-20 20:30:25 +0100

    feat(clojure): support cljstyle (#96)
    * feat: support cljstyle
    
    * ci: install cljstyle

---
+++ src/formatters/cljstyle.rs
@@ -0,0 +1,69 @@
+use super::execute_command;
+
+#[inline]
+pub fn format_using_cljstyle(
+    snippet_path: &std::path::Path,
+) -> std::io::Result<(bool, Option<String>)> {
+    let mut cmd = std::process::Command::new("cljstyle");
+
+    cmd.arg("fix").arg(snippet_path);
+
+    execute_command(&mut cmd, snippet_path)
+}
+
+#[cfg(test)]
+mod test_cljstyle {
+    use crate::{formatters::setup_snippet, languages::Language};
+
+    use super::format_using_cljstyle;
+
+    #[test_with::executable(cljstyle)]
+    #[test]
+    fn it_should_format_clojure() {
+        let input = "(  ns
+ foo.bar.baz  \"some doc\"
+    (:require (foo.bar [abc :as abc]
+        def))
+    (:use foo.bar.qux)
+    (:import foo.bar.qux.Foo
+      ;; Need this for the thing
+      foo.bar.qux.Bar)
+    )
+
+(defn hello \"says hi\" (
+      [] (hello \"world\")
+  ) ([name]
+  ( println \"Hello,\" name  )
+  ))";
+
+        let expected_output = "(ns foo.bar.baz
+  \"some doc\"
+  (:require
+    [foo.bar.abc :as abc]
+    [foo.bar.def]
+    [foo.bar.qux :refer :all])
+  (:import
+    (foo.bar.qux
+      ;; Need this for the thing
+      Bar
+      Foo)))
+
+
+(defn hello
+  \"says hi\"
+  ([] (hello \"world\"))
+  ([name]
+   (println \"Hello,\" name)))
+";
+
+        let snippet = setup_snippet(input, Language::Clojure.to_file_ext())
+            .expect("it to create a snippet file");
+
+        let output = format_using_cljstyle(snippet.path())
+            .expect("it to be successful")
+            .1
+            .expect("it to be some");
+
+        assert_eq!(output, expected_output);
+    }
+}
