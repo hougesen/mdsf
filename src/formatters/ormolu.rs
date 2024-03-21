2c84aabefc2319d40460b8f8aafeb1a2fac7ebf1
Mads Hougesen <mads@mhouge.dk>
2024-03-21 02:29:59 +0100

    feat(haskell): add support for ormolu (#102)

---
+++ src/formatters/ormolu.rs
@@ -0,0 +1,44 @@
+use super::execute_command;
+
+#[inline]
+pub fn format_using_ormolu(
+    snippet_path: &std::path::Path,
+) -> std::io::Result<(bool, Option<String>)> {
+    let mut cmd = std::process::Command::new("ormolu");
+
+    cmd.arg("--mode").arg("inplace").arg(snippet_path);
+
+    execute_command(&mut cmd, snippet_path)
+}
+
+#[cfg(test)]
+mod test_ormolu {
+    use crate::{formatters::setup_snippet, languages::Language};
+
+    use super::format_using_ormolu;
+
+    #[test_with::executable(ormolu)]
+    #[test]
+    fn it_should_format_haskell() {
+        let input = "
+addNumbers::Int->Int->Int
+addNumbers a b = do 
+        a + b 
+        ";
+
+        let expected_output = "addNumbers :: Int -> Int -> Int
+addNumbers a b = do
+  a + b
+";
+
+        let snippet = setup_snippet(input, Language::Haskell.to_file_ext())
+            .expect("it to create a snippet file");
+
+        let output = format_using_ormolu(snippet.path())
+            .expect("it to be successful")
+            .1
+            .expect("it to be some");
+
+        assert_eq!(output, expected_output);
+    }
+}
