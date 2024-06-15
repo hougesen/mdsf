67da7d420af5711359a9e0ae9f950a68a6691f2c
Mads Hougesen <mads@mhouge.dk>
2024-06-15 23:47:04 +0200

    feat: support dprint (#282)

---
+++ src/formatters/dprint.rs
@@ -0,0 +1,13 @@
+use super::execute_command;
+use crate::error::MdsfError;
+
+#[inline]
+pub fn format_using_dprint(
+    file_path: &std::path::Path,
+) -> Result<(bool, Option<String>), MdsfError> {
+    let mut cmd = std::process::Command::new("dprint");
+
+    cmd.arg("fmt").arg(file_path);
+
+    execute_command(&mut cmd, file_path)
+}
