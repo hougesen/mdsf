e48fdb83f1d1fd64704989ee88de6151c3150be6
Mads Hougesen <mads@mhouge.dk>
2024-06-16 02:01:33 +0200

    feat: support bsfmt

---
+++ src/formatters/bsfmt.rs
@@ -0,0 +1,13 @@
+use super::execute_command;
+use crate::error::MdsfError;
+
+#[inline]
+pub fn format_using_bsfmt(
+    file_path: &std::path::Path,
+) -> Result<(bool, Option<String>), MdsfError> {
+    let mut cmd = std::process::Command::new("bsfmt");
+
+    cmd.arg(file_path).arg("--write");
+
+    execute_command(&mut cmd, file_path)
+}
