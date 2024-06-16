3752a1fd2fa5fa964a7e8fbeaea0bab83cf4337b
Mads Hougesen <mads@mhouge.dk>
2024-06-16 02:27:00 +0200

    feat(dcm): add format and fix command

--- src/formatters/dcm.rs
+++ src/formatters/dcm.rs
@@ -2,7 +2,16 @@ use super::execute_command;
 use crate::error::MdsfError;
 
 #[inline]
-pub fn run(file_path: &std::path::Path) -> Result<(bool, Option<String>), MdsfError> {
+pub fn run_format(file_path: &std::path::Path) -> Result<(bool, Option<String>), MdsfError> {
+    let mut cmd = std::process::Command::new("dcm");
+
+    cmd.arg("format").arg(file_path);
+
+    execute_command(&mut cmd, file_path)
+}
+
+#[inline]
+pub fn run_fix(file_path: &std::path::Path) -> Result<(bool, Option<String>), MdsfError> {
     let mut cmd = std::process::Command::new("dcm");
 
     cmd.arg("fix").arg(file_path);
