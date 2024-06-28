dd48a711555c9d19aa9c9d5791c158f236a8b5a0
Mads Hougesen <mads@mhouge.dk>
2024-06-28 23:11:16 +0200

    test: css-beautify

--- src/formatters/css_beautify.rs
+++ src/formatters/css_beautify.rs
@@ -36,3 +36,31 @@ pub fn run(snippet_path: &std::path::Path) -> Result<(bool, Option<String>), Mds
 
     invoke_css_beautify(CommandType::Direct("css-beautify").build(), snippet_path)
 }
+
+#[cfg(test)]
+mod test_css_beautify {
+    use crate::{formatters::setup_snippet, generated::language_to_ext};
+
+    #[test_with::executable(css-beautify)]
+    fn it_should_format_css() {
+        let input = "h1   {color: blue;} p    {color: red;}";
+
+        let expected_output = "h1 {
+    color: blue;
+}
+
+p {
+    color: red;
+}";
+
+        let snippet =
+            setup_snippet(input, language_to_ext("css")).expect("it to create a snippet file");
+
+        let output = super::run(snippet.path())
+            .expect("it to be successful")
+            .1
+            .expect("it to be some");
+
+        assert_eq!(expected_output, output);
+    }
+}
