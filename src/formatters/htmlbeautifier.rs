1b2e39c29802eb0495656d91389400e1c7c7d1e6
Mads Hougesen <mads@mhouge.dk>
2024-06-28 23:20:14 +0200

    test: htmlbeautifier (#372)

--- src/formatters/htmlbeautifier.rs
+++ src/formatters/htmlbeautifier.rs
@@ -9,3 +9,34 @@ pub fn run(file_path: &std::path::Path) -> Result<(bool, Option<String>), MdsfEr
 
     execute_command(cmd, file_path)
 }
+
+#[cfg(test)]
+mod test_htmlbeautifier {
+    use crate::{formatters::setup_snippet, generated::language_to_ext};
+
+    #[test_with::executable(htmlbeautifier)]
+    fn it_should_format_html() {
+        let input = "<div>
+                    <p>
+                    Mads was here
+                    </p>
+        </div>";
+
+        let expected_output = "<div>
+  <p>
+    Mads was here
+  </p>
+</div>
+";
+
+        let snippet =
+            setup_snippet(input, language_to_ext("html")).expect("it to create a snippet file");
+
+        let output = super::run(snippet.path())
+            .expect("it to be successful")
+            .1
+            .expect("it to be some");
+
+        assert_eq!(expected_output, output);
+    }
+}
