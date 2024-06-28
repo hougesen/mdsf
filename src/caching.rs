05f8389888f48639ac7caa456810f0d2ebc82a99
Mads Hougesen <mads@mhouge.dk>
2024-06-28 15:06:06 +0200

    refactor: use sha256 instead of built in hash (#345)
    Switch to sha256 for cache key hashing

--- src/caching.rs
+++ src/caching.rs
@@ -1,21 +1,34 @@
 use std::hash::{DefaultHasher, Hash, Hasher};
 
+use sha2::{Digest, Sha256};
+
 use crate::config::MdsfConfig;
 
 #[inline]
 pub fn get_config_hash(config: &MdsfConfig) -> String {
-    let mut hasher = DefaultHasher::new();
+    serde_json::to_string(config).map_or_else(
+        |_error| {
+            let mut hasher = DefaultHasher::new();
 
-    config.hash(&mut hasher);
+            config.hash(&mut hasher);
 
-    format!("{}", hasher.finish())
+            format!("{}", hasher.finish())
+        },
+        |config_str| {
+            let mut h = Sha256::new();
+
+            h.update(config_str);
+
+            format!("{:X}", h.finalize())
+        },
+    )
 }
 
 #[inline]
 pub fn hash_text_block(text: &str) -> String {
-    let mut hasher = DefaultHasher::new();
+    let mut h = Sha256::new();
 
-    text.hash(&mut hasher);
+    h.update(text);
 
-    format!("{}", hasher.finish())
+    format!("{:X}", h.finalize())
 }
