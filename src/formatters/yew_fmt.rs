c046ae5535fe5ff19c42101e7eb96258bd7aacf8
Mads Hougesen <mads@mhouge.dk>
2024-06-28 22:41:48 +0200

    test: yew-fmt (#365)

--- src/formatters/yew_fmt.rs
+++ src/formatters/yew_fmt.rs
@@ -12,3 +12,29 @@ pub fn run(snippet_path: &std::path::Path) -> Result<(bool, Option<String>), Mds
 
     execute_command(cmd, snippet_path)
 }
+
+#[cfg(test)]
+mod test_yew_fmt {
+    use crate::{formatters::setup_snippet, generated::language_to_ext};
+
+    #[test_with::executable(yew-fmt)]
+    fn it_should_format_rust() {
+        let input = "pub
+                    async
+            fn    add( a: i32,
+                            b:i32 )->                   i32 {a+b}
+    ";
+
+        let expected_output = "pub async fn add(a: i32, b: i32) -> i32 {\n    a + b\n}\n";
+
+        let snippet =
+            setup_snippet(input, language_to_ext("rust")).expect("it to create a snippet file");
+
+        let output = super::run(snippet.path())
+            .expect("it to be successful")
+            .1
+            .expect("it to be some");
+
+        assert_eq!(expected_output, output);
+    }
+}
