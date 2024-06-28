2c7a49ea6a310f95986829332492a6ee1fd5552a
Mads Hougesen <mads@mhouge.dk>
2024-06-28 22:36:57 +0200

    test: grain format

--- src/formatters/grain.rs
+++ src/formatters/grain.rs
@@ -9,3 +9,31 @@ pub fn run_format(file_path: &std::path::Path) -> Result<(bool, Option<String>),
 
     execute_command(cmd, file_path)
 }
+
+#[cfg(test)]
+mod test_grain {
+    use crate::{formatters::setup_snippet, generated::language_to_ext};
+
+    #[test_with::executable(grain)]
+    fn it_should_format_grain() {
+        let input = "module Hello
+
+                                print(\"Hello, world!\")
+";
+
+        let expected_output = "module Hello
+
+print(\"Hello, world!\")
+";
+
+        let snippet =
+            setup_snippet(input, language_to_ext("grain")).expect("it to create a snippet file");
+
+        let output = super::run_format(snippet.path())
+            .expect("it to be successful")
+            .1
+            .expect("it to be some");
+
+        assert_eq!(expected_output, output);
+    }
+}
