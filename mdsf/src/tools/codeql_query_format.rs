0de89fd88799992fef168b77c45030c751d6c085
Mads Hougesen <mads@mhouge.dk>
2025-02-09 00:32:31 +0100

    feat(cli): support codeql query format (#677)

---
+++ mdsf/src/tools/codeql_query_format.rs
@@ -0,0 +1,21 @@
+///
+/// THIS FILE IS GENERATED USING CODE - DO NOT EDIT MANUALLY
+///
+use crate::runners::CommandType;
+
+#[inline]
+pub fn set_args(
+    mut cmd: std::process::Command,
+    file_path: &std::path::Path,
+) -> std::process::Command {
+    cmd.arg("query");
+    cmd.arg("format");
+    cmd.arg("-i");
+    cmd.arg(file_path);
+    cmd
+}
+
+pub const COMMANDS: [CommandType; 1] = [CommandType::Direct("codeql")];
+
+#[cfg(test)]
+mod test_codeql_query_format {}
