2ca16c838b50961f7544df4781cd3a5b11080ac9
Mads Hougesen <mads@mhouge.dk>
2025-02-13 22:49:24 +0100

    chore(cli): add homepages to more commands (#693)

---
+++ mdsf/src/tools/packer_validate.rs
@@ -0,0 +1,19 @@
+///
+/// THIS FILE IS GENERATED USING CODE - DO NOT EDIT MANUALLY
+///
+use crate::runners::CommandType;
+
+#[inline]
+pub fn set_args(
+    mut cmd: std::process::Command,
+    file_path: &std::path::Path,
+) -> std::process::Command {
+    cmd.arg("validate");
+    cmd.arg(file_path);
+    cmd
+}
+
+pub const COMMANDS: [CommandType; 1] = [CommandType::Direct("packer")];
+
+#[cfg(test)]
+mod test_packer_validate {}
---
+++ mdsf/src/tools/sqlfluff_lint.rs
@@ -0,0 +1,21 @@
+///
+/// THIS FILE IS GENERATED USING CODE - DO NOT EDIT MANUALLY
+///
+use crate::runners::CommandType;
+
+#[inline]
+pub fn set_args(
+    mut cmd: std::process::Command,
+    file_path: &std::path::Path,
+) -> std::process::Command {
+    cmd.arg("lint");
+    cmd.arg("--dialect");
+    cmd.arg("ansi");
+    cmd.arg(file_path);
+    cmd
+}
+
+pub const COMMANDS: [CommandType; 1] = [CommandType::Direct("sqlfluff")];
+
+#[cfg(test)]
+mod test_sqlfluff_lint {}
