e0f8a638ae77236521f968de9fd0e1a16bf155e9
Mads Hougesen <mads@mhouge.dk>
2025-02-24 20:14:30 +0100

    feat(tools): support write-good

---
+++ mdsf/src/tools/write_good.rs
@@ -0,0 +1,22 @@
+///
+/// THIS FILE IS GENERATED USING CODE - DO NOT EDIT MANUALLY
+///
+use crate::runners::CommandType;
+
+#[inline]
+pub fn set_args(
+    mut cmd: std::process::Command,
+    file_path: &std::path::Path,
+) -> std::process::Command {
+    cmd.arg(file_path);
+    cmd
+}
+
+pub const COMMANDS: [CommandType; 3] = [
+    CommandType::NodeModules("write-good"),
+    CommandType::Direct("write-good"),
+    CommandType::Npm("write-good"),
+];
+
+#[cfg(test)]
+mod test_write_good {}
