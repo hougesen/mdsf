17173df4ff3422686ec28daea882fd0d348082ea
Mads Hougesen <mads@mhouge.dk>
2025-03-02 15:46:20 +0100

    feat(tools): add support for cmake-lint (#768)
    Adds support for the cmake linter, cmake-lint, and adds package manager info to a few more plugins

--- mdsf/src/tools/cmake_lint.rs
+++ mdsf/src/tools/cmake_lint.rs
@@ -8,16 +8,10 @@ pub fn set_args(
     mut cmd: std::process::Command,
     file_path: &std::path::Path,
 ) -> std::process::Command {
-    cmd.arg("lint");
-    cmd.arg("-fix");
     cmd.arg(file_path);
     cmd
 }
 
-pub const COMMANDS: [CommandType; 3] = [
-    CommandType::NodeModules("protolint"),
-    CommandType::Direct("protolint"),
-    CommandType::Npm("protolint"),
-];
+pub const COMMANDS: [CommandType; 1] = [CommandType::Direct("cmake-lint")];
 
 pub const IS_STDIN: bool = false;
