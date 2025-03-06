0ee08c6a1915e4a70eff9f0046e1013cf2305bc7
Mads Hougesen <mads@mhouge.dk>
2025-03-06 04:01:56 +0100

    test: validate all JavaScriptRuntime can run commands

--- mdsf/src/runners/deno.rs
+++ mdsf/src/runners/deno.rs
@@ -6,3 +6,28 @@ pub fn new_deno_cmd(package_name: &str) -> std::process::Command {
 
     cmd
 }
+
+#[cfg(test)]
+mod test_deno {
+    use crate::tools::Tooling;
+
+    #[test_with::executable(deno)]
+    #[test]
+    fn it_can_execute_an_npm_package_script() {
+        let input = "";
+
+        let file_ext = crate::fttype::get_file_extension("json");
+
+        let snippet =
+            crate::execution::setup_snippet(input, &file_ext).expect("it to create a snippet file");
+
+        Tooling::Prettier
+            .format_snippet(
+                snippet.path(),
+                crate::testing::DEFAULT_TEST_FORMATTER_TIMEOUT,
+                crate::testing::DEFAULT_TEST_DEBUG_ENABLED,
+                crate::runners::JavaScriptRuntime::Deno,
+            )
+            .expect("it to succeed");
+    }
+}
