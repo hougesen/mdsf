f3b087cf9ee45397cb096f8003e79271fcc101d6
Mads Hougesen <mads@mhouge.dk>
2025-03-06 23:03:25 +0100

    refactor(tools): convert xo to stdin based args (#797)

--- mdsf/src/tools/xo.rs
+++ mdsf/src/tools/xo.rs
@@ -6,10 +6,10 @@ use crate::runners::CommandType;
 #[inline]
 pub fn set_args(
     mut cmd: std::process::Command,
-    file_path: &std::path::Path,
+    _file_path: &std::path::Path,
 ) -> std::process::Command {
     cmd.arg("--fix");
-    cmd.arg(file_path);
+    cmd.arg("--stdin");
     cmd
 }
 
@@ -19,4 +19,42 @@ pub const COMMANDS: [CommandType; 3] = [
     CommandType::Npm("xo"),
 ];
 
-pub const IS_STDIN: bool = false;
+pub const IS_STDIN: bool = true;
+
+#[cfg(test)]
+mod test_xo {
+    #[test_with::executable(npx)]
+    fn test_xo_javascript_77a8cbfa8cbcea9d() {
+        let input = r#"    function asyncAddition(a,b  )
+    {
+        return a+b
+    }
+
+                    console.info(asyncAddition(1, 2));"#;
+
+        let output = r#"function asyncAddition(a, b) {
+	return a + b;
+}
+
+console.info(asyncAddition(1, 2));
+"#;
+
+        let file_ext = crate::fttype::get_file_extension("javascript");
+
+        let snippet =
+            crate::execution::setup_snippet(input, &file_ext).expect("it to create a snippet file");
+
+        let result = crate::tools::Tooling::Xo
+            .format_snippet(
+                snippet.path(),
+                crate::testing::DEFAULT_TEST_FORMATTER_TIMEOUT,
+                crate::testing::DEFAULT_TEST_DEBUG_ENABLED,
+                crate::runners::JavaScriptRuntime::default(),
+            )
+            .expect("it to be successful")
+            .1
+            .expect("it to be some");
+
+        assert_eq!(result, output);
+    }
+}
