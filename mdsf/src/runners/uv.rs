af17c92787cd67cda363b3e4954b5aea4622660d
Mads Hougesen <mads@mhouge.dk>
2025-03-08 20:23:22 +0100

    fix: 'uv tool run' instead of 'uv run' (#813)

--- mdsf/src/runners/uv.rs
+++ mdsf/src/runners/uv.rs
@@ -2,6 +2,7 @@
 pub fn setup_uv_run_command(package_name: &str) -> std::process::Command {
     let mut command = std::process::Command::new("uv");
 
+    command.arg("tool");
     command.arg("run");
     command.arg("--no-progress");
     command.arg(package_name);
