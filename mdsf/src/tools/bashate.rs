bcb1dbed10583a5d5527af482ea453f8b0798b03
Mads Hougesen <mads@mhouge.dk>
2025-03-09 15:35:14 +0100

    test(tools): validate bashate (#818)

--- mdsf/src/tools/bashate.rs
+++ mdsf/src/tools/bashate.rs
@@ -19,3 +19,83 @@ pub const COMMANDS: [CommandType; 3] = [
 ];
 
 pub const IS_STDIN: bool = false;
+
+#[cfg(test)]
+mod test_bashate {
+    #[test_with::executable(bashate || pipx || uv)]
+    fn test_bashate_bash_1f0c485b85eb22b1() {
+        let input = r#"# for loop examples
+
+# pass
+for i in $(seq 1 5); do
+    echo $i
+done
+
+# pass
+for (( i = 0 ; i < 5 ; i++ )); do
+    echo $i
+done
+
+# fail E010
+for i in $(seq 1 5); do
+    echo $i
+done
+
+# fail E010
+for (( i = 0 ; i < 5 ; i++ )); do
+    echo $i
+done
+
+awk '{
+    for (i = 1; i < 5; i++)
+        print $i
+}' < /dev/null
+"#;
+
+        let output = r#"# for loop examples
+
+# pass
+for i in $(seq 1 5); do
+    echo $i
+done
+
+# pass
+for (( i = 0 ; i < 5 ; i++ )); do
+    echo $i
+done
+
+# fail E010
+for i in $(seq 1 5); do
+    echo $i
+done
+
+# fail E010
+for (( i = 0 ; i < 5 ; i++ )); do
+    echo $i
+done
+
+awk '{
+    for (i = 1; i < 5; i++)
+        print $i
+}' < /dev/null
+"#;
+
+        let file_ext = crate::fttype::get_file_extension("bash");
+
+        let snippet =
+            crate::execution::setup_snippet(input, &file_ext).expect("it to create a snippet file");
+
+        let result = crate::tools::Tooling::Bashate
+            .format_snippet(
+                snippet.path(),
+                crate::testing::DEFAULT_TEST_FORMATTER_TIMEOUT,
+                crate::testing::DEFAULT_TEST_DEBUG_ENABLED,
+                &crate::config::MdsfConfigRunners::all(),
+            )
+            .expect("it to be successful")
+            .1
+            .expect("it to be some");
+
+        assert_eq!(result, output);
+    }
+}
