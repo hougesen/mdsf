796f7712f9b4757cc01b071257e679edc60d7e36
Mads Hougesen <mads@mhouge.dk>
2025-03-10 19:28:00 +0100

    test(tools): validate ufmt works (#834)

--- mdsf/src/tools/ufmt.rs
+++ mdsf/src/tools/ufmt.rs
@@ -20,3 +20,33 @@ pub const COMMANDS: [CommandType; 3] = [
 ];
 
 pub const IS_STDIN: bool = false;
+
+#[cfg(test)]
+mod test_ufmt {
+    #[test_with::executable(ufmt || pipx || uv)]
+    fn test_ufmt_python_229ec2b01c2bfe3c() {
+        let input = r#"def add( a: int ,  b:int)->int: return a+b"#;
+
+        let output = r#"def add(a: int, b: int) -> int:
+    return a + b
+"#;
+
+        let file_ext = crate::fttype::get_file_extension("python");
+
+        let snippet =
+            crate::execution::setup_snippet(input, &file_ext).expect("it to create a snippet file");
+
+        let result = crate::tools::Tooling::Ufmt
+            .format_snippet(
+                snippet.path(),
+                crate::testing::DEFAULT_TEST_FORMATTER_TIMEOUT,
+                crate::testing::DEFAULT_TEST_DEBUG_ENABLED,
+                &crate::config::MdsfConfigRunners::all(),
+            )
+            .expect("it to be successful")
+            .1
+            .expect("it to be some");
+
+        assert_eq!(result, output);
+    }
+}
