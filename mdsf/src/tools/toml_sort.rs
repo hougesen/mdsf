ebacb10e8111d03254b0bb839952bdb0528c1b14
Mads Hougesen <mads@mhouge.dk>
2025-03-10 19:42:21 +0100

    test(tools): validate toml-sort (#836)

--- mdsf/src/tools/toml_sort.rs
+++ mdsf/src/tools/toml_sort.rs
@@ -20,3 +20,51 @@ pub const COMMANDS: [CommandType; 3] = [
 ];
 
 pub const IS_STDIN: bool = false;
+
+#[cfg(test)]
+mod test_toml_sort {
+    #[test_with::executable(toml-sort || pipx || uv)]
+    fn test_toml_sort_toml_8c2b58a6580e9412() {
+        let input = r#"
+
+[c]
+key = "something"
+
+
+[a]
+key = "something"
+
+[b]
+key = "something"
+
+"#;
+
+        let output = r#"[a]
+key = "something"
+
+[b]
+key = "something"
+
+[c]
+key = "something"
+"#;
+
+        let file_ext = crate::fttype::get_file_extension("toml");
+
+        let snippet =
+            crate::execution::setup_snippet(input, &file_ext).expect("it to create a snippet file");
+
+        let result = crate::tools::Tooling::TomlSort
+            .format_snippet(
+                snippet.path(),
+                crate::testing::DEFAULT_TEST_FORMATTER_TIMEOUT,
+                crate::testing::DEFAULT_TEST_DEBUG_ENABLED,
+                &crate::config::MdsfConfigRunners::all(),
+            )
+            .expect("it to be successful")
+            .1
+            .expect("it to be some");
+
+        assert_eq!(result, output);
+    }
+}
