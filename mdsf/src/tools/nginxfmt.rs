cf83e54bb2baf5d0b8e7fe876d894f7045596812
Mads Hougesen <mads@mhouge.dk>
2025-03-22 21:18:05 +0100

    test(tools): add test for nginxfmt

--- mdsf/src/tools/nginxfmt.rs
+++ mdsf/src/tools/nginxfmt.rs
@@ -19,3 +19,41 @@ pub const COMMANDS: [CommandType; 3] = [
 ];
 
 pub const IS_STDIN: bool = false;
+
+#[cfg(test)]
+mod test_nginxfmt {
+    #[test_with::executable(nginxfmt || pipx || uv)]
+    fn test_nginxfmt_conf_2e651ac1789b7182() {
+        let input = r#"server {
+    listen 80;
+        listen [::]:80;
+       server_name example.com;
+    }
+"#;
+
+        let output = r#"server {
+    listen 80;
+    listen [::]:80;
+    server_name example.com;
+}
+"#;
+
+        let file_ext = crate::fttype::get_file_extension(".conf");
+
+        let snippet =
+            crate::execution::setup_snippet(input, &file_ext).expect("it to create a snippet file");
+
+        let result = crate::tools::Tooling::Nginxfmt
+            .format_snippet(
+                snippet.path(),
+                crate::testing::DEFAULT_TEST_FORMATTER_TIMEOUT,
+                crate::testing::DEFAULT_TEST_DEBUG_ENABLED,
+                &crate::config::MdsfConfigRunners::all(),
+            )
+            .expect("it to be successful")
+            .1
+            .expect("it to be some");
+
+        assert_eq!(result, output);
+    }
+}
