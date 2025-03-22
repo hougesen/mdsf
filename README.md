f2f2c1b686f401e42bff7a4f166af24fe6b2a598
Mads Hougesen <mads@mhouge.dk>
2025-03-22 17:31:07 +0100

    test(tools): validate biome:lint and biome:lint:unsafe (#852)

--- README.md
+++ README.md
@@ -146,7 +146,7 @@ conform.setup({
 <!-- START_SECTION:base-command-help -->
 
 ```
-mdsf 0.8.2
+mdsf 0.8.2-next
 Format, and lint, markdown code snippets using your favorite tools
 Mads Hougesen <mads@mhouge.dk>
 
--- mdsf/src/tools/biome_lint.rs
+++ mdsf/src/tools/biome_lint.rs
@@ -25,3 +25,33 @@ pub const COMMANDS: [CommandType; 7] = [
 ];
 
 pub const IS_STDIN: bool = false;
+
+#[cfg(test)]
+mod test_biome_lint {
+    #[test_with::executable(biome || npx || pnpm || deno || bunx)]
+    fn test_biome_lint_javascript_3b1c1d6fd9c2e176() {
+        let input = r#"let variable = 0;
+"#;
+
+        let output = r#"const variable = 0;
+"#;
+
+        let file_ext = crate::fttype::get_file_extension("javascript");
+
+        let snippet =
+            crate::execution::setup_snippet(input, &file_ext).expect("it to create a snippet file");
+
+        let result = crate::tools::Tooling::BiomeLint
+            .format_snippet(
+                snippet.path(),
+                crate::testing::DEFAULT_TEST_FORMATTER_TIMEOUT,
+                crate::testing::DEFAULT_TEST_DEBUG_ENABLED,
+                &crate::config::MdsfConfigRunners::all(),
+            )
+            .expect("it to be successful")
+            .1
+            .expect("it to be some");
+
+        assert_eq!(result, output);
+    }
+}
