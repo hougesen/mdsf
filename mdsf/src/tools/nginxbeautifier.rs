efddd0ef65ac59728ac3198aad1cf61c0ae7cfc9
Mads Hougesen <mads@mhouge.dk>
2025-03-22 21:10:22 +0100

    test(tools): validate nginxbeautifier

--- mdsf/src/tools/nginxbeautifier.rs
+++ mdsf/src/tools/nginxbeautifier.rs
@@ -23,3 +23,41 @@ pub const COMMANDS: [CommandType; 7] = [
 ];
 
 pub const IS_STDIN: bool = false;
+
+#[cfg(test)]
+mod test_nginxbeautifier {
+    #[test_with::executable(nginxbeautifier || npx || pnpm || deno || bunx)]
+    fn test_nginxbeautifier_conf_5c2a2e0d4f44354f() {
+        let input = r#"server {
+    listen 80;
+        listen [::]:80;
+       server_name example.com;
+    }
+"#;
+
+        let output = r#"server {
+	listen 80;
+	listen [::]:80;
+	server_name example.com;
+}
+"#;
+
+        let file_ext = crate::fttype::get_file_extension(".conf");
+
+        let snippet =
+            crate::execution::setup_snippet(input, &file_ext).expect("it to create a snippet file");
+
+        let result = crate::tools::Tooling::Nginxbeautifier
+            .format_snippet(
+                snippet.path(),
+                crate::testing::DEFAULT_TEST_FORMATTER_TIMEOUT,
+                crate::testing::DEFAULT_TEST_DEBUG_ENABLED,
+                &crate::config::MdsfConfigRunners::all(),
+            )
+            .expect("it to be successful")
+            .1
+            .expect("it to be some");
+
+        assert_eq!(result, output);
+    }
+}
