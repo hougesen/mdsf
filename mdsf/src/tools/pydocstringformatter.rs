17e3fb267f0fa945aeadba070a66065dfb1891cf
Mads Hougesen <mads@mhouge.dk>
2025-03-23 04:59:31 +0100

    feat(tools): add support for pydocstringformatter (#873)

---
+++ mdsf/src/tools/pydocstringformatter.rs
@@ -0,0 +1,22 @@
+///
+/// THIS FILE IS GENERATED USING CODE - DO NOT EDIT MANUALLY
+///
+use crate::runners::CommandType;
+
+#[inline]
+pub fn set_args(
+    mut cmd: std::process::Command,
+    file_path: &std::path::Path,
+) -> std::process::Command {
+    cmd.arg("-w");
+    cmd.arg(file_path);
+    cmd
+}
+
+pub const COMMANDS: [CommandType; 3] = [
+    CommandType::Direct("pydocstringformatter"),
+    CommandType::Uv("pydocstringformatter", "pydocstringformatter"),
+    CommandType::Pipx("pydocstringformatter"),
+];
+
+pub const IS_STDIN: bool = false;
