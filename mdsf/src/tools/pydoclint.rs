6c08e455c2cfe9725c789f74b3e375412043c9d0
Mads Hougesen <mads@mhouge.dk>
2025-03-23 05:06:18 +0100

    feat(tools): add support for pydoclint (#875)

---
+++ mdsf/src/tools/pydoclint.rs
@@ -0,0 +1,21 @@
+///
+/// THIS FILE IS GENERATED USING CODE - DO NOT EDIT MANUALLY
+///
+use crate::runners::CommandType;
+
+#[inline]
+pub fn set_args(
+    mut cmd: std::process::Command,
+    file_path: &std::path::Path,
+) -> std::process::Command {
+    cmd.arg(file_path);
+    cmd
+}
+
+pub const COMMANDS: [CommandType; 3] = [
+    CommandType::Direct("pydoclint"),
+    CommandType::Uv("pydoclint", "pydoclint"),
+    CommandType::Pipx("pydoclint"),
+];
+
+pub const IS_STDIN: bool = false;
