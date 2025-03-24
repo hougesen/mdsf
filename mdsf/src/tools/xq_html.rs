1045e509878f940b4870f293488ad498c7cd62e8
Mads Hougesen <mads@mhouge.dk>
2025-03-24 19:52:45 +0100

    test(tools): add test for xq:html

--- mdsf/src/tools/xq_html.rs
+++ mdsf/src/tools/xq_html.rs
@@ -15,3 +15,34 @@ pub fn set_args(
 pub const COMMANDS: [CommandType; 1] = [CommandType::Direct("xq")];
 
 pub const IS_STDIN: bool = true;
+
+#[cfg(test)]
+mod test_xq_html {
+    #[test_with::executable(xq)]
+    fn test_xq_html_html_aa297be5c685b338() {
+        let input = r#"<div> 
+         </div>"#;
+
+        let output = r#"<div>
+</div>
+"#;
+
+        let file_ext = crate::fttype::get_file_extension("html");
+
+        let snippet =
+            crate::execution::setup_snippet(input, &file_ext).expect("it to create a snippet file");
+
+        let result = crate::tools::Tooling::XqHtml
+            .format_snippet(
+                snippet.path(),
+                crate::testing::DEFAULT_TEST_FORMATTER_TIMEOUT,
+                crate::testing::DEFAULT_TEST_DEBUG_ENABLED,
+                &crate::config::MdsfConfigRunners::all(),
+            )
+            .expect("it to be successful")
+            .1
+            .expect("it to be some");
+
+        assert_eq!(result, output);
+    }
+}
