df3df8523a9c407825369fa40a4563ea9cc410d2
Mads Hougesen <mads@mhouge.dk>
2025-03-24 04:18:48 +0100

    test(tools): validate shellcheck works

--- mdsf/src/tools/shellcheck.rs
+++ mdsf/src/tools/shellcheck.rs
@@ -15,3 +15,37 @@ pub fn set_args(
 pub const COMMANDS: [CommandType; 1] = [CommandType::Direct("shellcheck")];
 
 pub const IS_STDIN: bool = false;
+
+#[cfg(test)]
+mod test_shellcheck {
+    #[test_with::executable(shellcheck)]
+    fn test_shellcheck_shell_7176996a1b8efe54() {
+        let input = r#"#!/bin/sh
+
+echo "Hello World"
+"#;
+
+        let output = r#"#!/bin/sh
+
+echo "Hello World"
+"#;
+
+        let file_ext = crate::fttype::get_file_extension("shell");
+
+        let snippet =
+            crate::execution::setup_snippet(input, &file_ext).expect("it to create a snippet file");
+
+        let result = crate::tools::Tooling::Shellcheck
+            .format_snippet(
+                snippet.path(),
+                crate::testing::DEFAULT_TEST_FORMATTER_TIMEOUT,
+                crate::testing::DEFAULT_TEST_DEBUG_ENABLED,
+                &crate::config::MdsfConfigRunners::all(),
+            )
+            .expect("it to be successful")
+            .1
+            .expect("it to be some");
+
+        assert_eq!(result, output);
+    }
+}
