2fd11f48c54d2dd71f50085f3383a1c9ed46c550
Mads Hougesen <mads@mhouge.dk>
2025-04-04 21:07:15 +0200

    test(tools): add test for wfindent (#914)

--- mdsf/src/tools/wfindent.rs
+++ mdsf/src/tools/wfindent.rs
@@ -15,3 +15,57 @@ pub fn set_args(
 pub const COMMANDS: [CommandType; 1] = [CommandType::Direct("wfindent")];
 
 pub const IS_STDIN: bool = false;
+
+#[cfg(test)]
+mod test_wfindent {
+    #[test_with::executable(wfindent)]
+    fn test_wfindent_fortran_a51b7de807928738() {
+        let input = r#"program demo
+integer :: endif,if,elseif
+integer,DIMENSION(2) :: function
+endif=3;if=2
+if(endif==2)then
+endif=5
+elseif=if+4*(endif+&
+2**10)
+elseif(endif==3)then
+function(if)=endif/elseif
+print*,endif
+endif
+end program
+"#;
+
+        let output = r#"program demo
+   integer :: endif,if,elseif
+   integer,DIMENSION(2) :: function
+   endif=3;if=2
+   if(endif==2)then
+      endif=5
+      elseif=if+4*(endif+&
+         2**10)
+   elseif(endif==3)then
+      function(if)=endif/elseif
+      print*,endif
+   endif
+end program
+"#;
+
+        let file_ext = crate::fttype::get_file_extension("fortran");
+
+        let snippet =
+            crate::execution::setup_snippet(input, &file_ext).expect("it to create a snippet file");
+
+        let result = crate::tools::Tooling::Wfindent
+            .format_snippet(
+                snippet.path(),
+                crate::testing::DEFAULT_TEST_FORMATTER_TIMEOUT,
+                crate::testing::DEFAULT_TEST_DEBUG_ENABLED,
+                &crate::config::MdsfConfigRunners::all(),
+            )
+            .expect("it to be successful")
+            .1
+            .expect("it to be some");
+
+        assert_eq!(result, output);
+    }
+}
