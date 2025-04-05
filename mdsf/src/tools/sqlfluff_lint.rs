f46eaea22c0092c9620cb30fa2b93093c5b59f36
Mads Hougesen <mads@mhouge.dk>
2025-04-05 19:04:18 +0200

    test(tools): validate sqlfluff:lint and sqlfluff:fix

--- mdsf/src/tools/sqlfluff_lint.rs
+++ mdsf/src/tools/sqlfluff_lint.rs
@@ -24,3 +24,35 @@ pub const COMMANDS: [CommandType; 3] = [
 ];
 
 pub const IS_STDIN: bool = false;
+
+#[cfg(test)]
+mod test_sqlfluff_lint {
+    #[test_with::executable(sqlfluff || pipx || uv)]
+    fn test_sqlfluff_lint_sql_9ec8d0c58d20cc30() {
+        let input = r#"SELECT asd FROM tbl
+WHERE foo = 'bar' LIMIT 10;
+"#;
+
+        let output = r#"SELECT asd FROM tbl
+WHERE foo = 'bar' LIMIT 10;
+"#;
+
+        let file_ext = crate::fttype::get_file_extension("sql");
+
+        let snippet =
+            crate::execution::setup_snippet(input, &file_ext).expect("it to create a snippet file");
+
+        let result = crate::tools::Tooling::SqlfluffLint
+            .format_snippet(
+                snippet.path(),
+                crate::testing::DEFAULT_TEST_FORMATTER_TIMEOUT,
+                crate::testing::DEFAULT_TEST_DEBUG_ENABLED,
+                &crate::config::MdsfConfigRunners::all(),
+            )
+            .expect("it to be successful")
+            .1
+            .expect("it to be some");
+
+        assert_eq!(result, output);
+    }
+}
