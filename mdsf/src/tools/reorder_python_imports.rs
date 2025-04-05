9f977333d3e215059daf8fbe6e6f6eb3c29adc93
hougesen <hougesen@users.noreply.github.com>
2025-04-05 17:18:12 +0000

    chore: updated generated code

--- mdsf/src/tools/reorder_python_imports.rs
+++ mdsf/src/tools/reorder_python_imports.rs
@@ -19,3 +19,37 @@ pub const COMMANDS: [CommandType; 3] = [
 ];
 
 pub const IS_STDIN: bool = false;
+
+#[cfg(test)]
+mod test_reorder_python_imports {
+    #[test_with::executable(reorder-python-imports || pipx || uv)]
+    fn test_reorder_python_imports_python_27ed7b74e180e8af() {
+        let input = r#"import sys
+import pyramid
+import reorder_python_imports"#;
+
+        let output = r#"import sys
+
+import pyramid
+
+import reorder_python_imports"#;
+
+        let file_ext = crate::fttype::get_file_extension("python");
+
+        let snippet =
+            crate::execution::setup_snippet(input, &file_ext).expect("it to create a snippet file");
+
+        let result = crate::tools::Tooling::ReorderPythonImports
+            .format_snippet(
+                snippet.path(),
+                crate::testing::DEFAULT_TEST_FORMATTER_TIMEOUT,
+                crate::testing::DEFAULT_TEST_DEBUG_ENABLED,
+                &crate::config::MdsfConfigRunners::all(),
+            )
+            .expect("it to be successful")
+            .1
+            .expect("it to be some");
+
+        assert_eq!(result, output);
+    }
+}
