b8484cb479c6c9a580a45b66ad84f377a5aad3d4
hougesen <hougesen@users.noreply.github.com>
2025-04-05 17:16:32 +0000

    chore: updated generated code

--- mdsf/src/tools/sqlfmt.rs
+++ mdsf/src/tools/sqlfmt.rs
@@ -19,3 +19,35 @@ pub const COMMANDS: [CommandType; 3] = [
 ];
 
 pub const IS_STDIN: bool = false;
+
+#[cfg(test)]
+mod test_sqlfmt {
+    #[test_with::executable(sqlfmt || pipx || uv)]
+    fn test_sqlfmt_sql_7933045821741e3() {
+        let input = r#"SELECT  *                  FROM  tbl                        WHERE                      foo   = 'bar';"#;
+
+        let output = r#"select *
+from tbl
+where foo = 'bar'
+;
+"#;
+
+        let file_ext = crate::fttype::get_file_extension("sql");
+
+        let snippet =
+            crate::execution::setup_snippet(input, &file_ext).expect("it to create a snippet file");
+
+        let result = crate::tools::Tooling::Sqlfmt
+            .format_snippet(
+                snippet.path(),
+                crate::testing::DEFAULT_TEST_FORMATTER_TIMEOUT,
+                crate::testing::DEFAULT_TEST_DEBUG_ENABLED,
+                &crate::config::MdsfConfigRunners::all(),
+            )
+            .expect("it to be successful")
+            .1
+            .expect("it to be some");
+
+        assert_eq!(result, output);
+    }
+}
