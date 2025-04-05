ae56b79734919609851d9bd5c73f1d81bbcc9654
hougesen <hougesen@users.noreply.github.com>
2025-04-05 17:17:37 +0000

    chore: updated generated code

--- mdsf/src/tools/rustywind.rs
+++ mdsf/src/tools/rustywind.rs
@@ -24,3 +24,33 @@ pub const COMMANDS: [CommandType; 7] = [
 ];
 
 pub const IS_STDIN: bool = false;
+
+#[cfg(test)]
+mod test_rustywind {
+    #[test_with::executable(rustywind || npx || pnpm || deno || bunx)]
+    fn test_rustywind_html_f482eb2ece82bb0d() {
+        let input = r#"<div class="flex-col flex"></div>
+"#;
+
+        let output = r#"<div class="flex flex-col"></div>
+"#;
+
+        let file_ext = crate::fttype::get_file_extension("html");
+
+        let snippet =
+            crate::execution::setup_snippet(input, &file_ext).expect("it to create a snippet file");
+
+        let result = crate::tools::Tooling::Rustywind
+            .format_snippet(
+                snippet.path(),
+                crate::testing::DEFAULT_TEST_FORMATTER_TIMEOUT,
+                crate::testing::DEFAULT_TEST_DEBUG_ENABLED,
+                &crate::config::MdsfConfigRunners::all(),
+            )
+            .expect("it to be successful")
+            .1
+            .expect("it to be some");
+
+        assert_eq!(result, output);
+    }
+}
