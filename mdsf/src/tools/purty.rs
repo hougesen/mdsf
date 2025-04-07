2487674e95218c06d377112fcae8d86554fbddf5
Mads Hougesen <mads@mhouge.dk>
2025-04-07 19:16:28 +0200

    test(tools): validate purty (#929)

--- mdsf/src/tools/purty.rs
+++ mdsf/src/tools/purty.rs
@@ -24,3 +24,42 @@ pub const COMMANDS: [CommandType; 7] = [
 ];
 
 pub const IS_STDIN: bool = false;
+
+#[cfg(test)]
+mod test_purty {
+    #[test_with::executable(purty || npx || pnpm || deno || bunx)]
+    fn test_purty_purescript_37730dad0a7f9fbd() {
+        let input = r#"module Mdsf where
+
+
+
+
+add   :: Int -> Int  ->    Int
+add a   b = a +         b
+"#;
+
+        let output = r#"module Mdsf where
+
+add :: Int -> Int -> Int
+add a b = a + b
+"#;
+
+        let file_ext = crate::fttype::get_file_extension("purescript");
+
+        let snippet =
+            crate::execution::setup_snippet(input, &file_ext).expect("it to create a snippet file");
+
+        let result = crate::tools::Tooling::Purty
+            .format_snippet(
+                snippet.path(),
+                crate::testing::DEFAULT_TEST_FORMATTER_TIMEOUT,
+                crate::testing::DEFAULT_TEST_DEBUG_ENABLED,
+                &crate::config::MdsfConfigRunners::all(),
+            )
+            .expect("it to be successful")
+            .1
+            .expect("it to be some");
+
+        assert_eq!(result, output);
+    }
+}
