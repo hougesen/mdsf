e7e06d3e061b700d5bbce038a8d833ed95d7fa09
Mads Hougesen <mads@mhouge.dk>
2025-04-09 21:04:16 +0200

    test(tools): validate csslint (#945)

--- mdsf/src/tools/csslint.rs
+++ mdsf/src/tools/csslint.rs
@@ -24,3 +24,37 @@ pub const COMMANDS: [CommandType; 7] = [
 ];
 
 pub const IS_STDIN: bool = false;
+
+#[cfg(test)]
+mod test_csslint {
+    #[test_with::executable(csslint || npx || pnpm || deno || bunx)]
+    fn test_csslint_css_9b7fd0554eb344f() {
+        let input = r#"body {
+  background: red;
+}
+"#;
+
+        let output = r#"body {
+  background: red;
+}
+"#;
+
+        let file_ext = crate::fttype::get_file_extension("css");
+
+        let snippet =
+            crate::execution::setup_snippet(input, &file_ext).expect("it to create a snippet file");
+
+        let result = crate::tools::Tooling::Csslint
+            .format_snippet(
+                snippet.path(),
+                crate::testing::DEFAULT_TEST_FORMATTER_TIMEOUT,
+                crate::testing::DEFAULT_TEST_DEBUG_ENABLED,
+                &crate::config::MdsfConfigRunners::all(),
+            )
+            .expect("it to be successful")
+            .1
+            .expect("it to be some");
+
+        assert_eq!(result, output);
+    }
+}
