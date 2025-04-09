f8221555cda79011c918e1cd51f83aabf918b1b6
Mads Hougesen <mads@mhouge.dk>
2025-04-09 14:07:04 +0200

    test: verify --stdin with formatted code

--- mdsf/tests/main.rs
+++ mdsf/tests/main.rs
@@ -1,114 +1,130 @@
-use std::io::Write;
+#[cfg(test)]
+mod test_cli {
+    use std::io::Write;
 
-use assert_cmd::{assert::OutputAssertExt, cargo::CommandCargoExt};
-use tempfile::tempdir;
+    use tempfile::tempdir;
 
-const BROKEN_CODE: &'static str = "```rust
+    const BROKEN_CODE: &'static str = "```rust
 fn add(a:i32,b:i32)->i32{a+b}
 ```
 ";
 
-const FORMATTED_CODE: &'static str = "```rust
+    const FORMATTED_CODE: &'static str = "```rust
 fn add(a: i32, b: i32) -> i32 {
     a + b
 }
 ```
 ";
 
-fn mdsf_command(path: &std::path::Path) -> std::process::Command {
-    let mut cmd = std::process::Command::cargo_bin("mdsf").unwrap();
+    fn mdsf_command(path: &std::path::Path) -> assert_cmd::Command {
+        let mut cmd = assert_cmd::Command::cargo_bin("mdsf").unwrap();
 
-    cmd.current_dir(path);
+        cmd.current_dir(path);
 
-    cmd
-}
+        cmd
+    }
 
-fn setup_test_input(dir: &std::path::Path, code: &str) -> tempfile::NamedTempFile {
-    let mut b = tempfile::Builder::new();
+    fn setup_test_input(dir: &std::path::Path, code: &str) -> tempfile::NamedTempFile {
+        let mut b = tempfile::Builder::new();
 
-    b.rand_bytes(12).suffix(".md");
+        b.rand_bytes(12).suffix(".md");
 
-    let mut f = b.tempfile_in(dir).unwrap();
+        let mut f = b.tempfile_in(dir).unwrap();
 
-    f.write_all(code.as_bytes()).unwrap();
-    f.flush().unwrap();
+        f.write_all(code.as_bytes()).unwrap();
+        f.flush().unwrap();
 
-    f
-}
+        f
+    }
 
-#[test]
-fn validate_help_command() {
-    let dir = tempdir().unwrap();
+    #[test]
+    fn validate_help_command() {
+        let dir = tempdir().unwrap();
 
-    let cmd = mdsf_command(dir.path()).arg("--help").assert().success();
-
-    let output = cmd.get_output();
-
-    assert!(!output.stdout.is_empty());
-}
-
-#[test]
-fn validate_completions_command() {
-    let dir = tempdir().unwrap();
-
-    let shells = ["bash", "elvish", "fish", "nushell", "powershell", "zsh"];
-
-    for shell in shells {
-        let cmd = mdsf_command(dir.path())
-            .arg("completions")
-            .arg(shell)
-            .assert()
-            .success();
+        let cmd = mdsf_command(dir.path()).arg("--help").assert().success();
 
         let output = cmd.get_output();
 
         assert!(!output.stdout.is_empty());
     }
-}
 
-#[test]
-fn validate_cache_prune_command() {
-    let dir = tempdir().unwrap();
+    #[test]
+    fn validate_completions_command() {
+        let dir = tempdir().unwrap();
 
-    mdsf_command(dir.path())
-        .arg("cache-prune")
-        .assert()
-        .success();
-}
+        let shells = ["bash", "elvish", "fish", "nushell", "powershell", "zsh"];
 
-#[test]
-fn validate_check_command_with_formatted_input() {
-    let dir = tempdir().unwrap();
+        for shell in shells {
+            let cmd = mdsf_command(dir.path())
+                .arg("completions")
+                .arg(shell)
+                .assert()
+                .success();
 
-    mdsf_command(dir.path()).arg("init").assert().success();
+            let output = cmd.get_output();
 
-    let file = setup_test_input(dir.path(), FORMATTED_CODE);
+            assert!(!output.stdout.is_empty());
+        }
+    }
 
-    mdsf_command(dir.path())
-        .arg("verify")
-        .arg(file.path())
-        .assert()
-        .success();
-}
+    #[test]
+    fn validate_cache_prune_command() {
+        let dir = tempdir().unwrap();
 
-#[test]
-fn validate_check_command_with_broken_input() {
-    let dir = tempdir().unwrap();
+        mdsf_command(dir.path())
+            .arg("cache-prune")
+            .assert()
+            .success();
+    }
 
-    mdsf_command(dir.path()).arg("init").assert().success();
+    #[test]
+    fn validate_check_command_with_formatted_input() {
+        let dir = tempdir().unwrap();
 
-    let file = setup_test_input(dir.path(), BROKEN_CODE);
+        mdsf_command(dir.path()).arg("init").assert().success();
 
-    mdsf_command(dir.path())
-        .arg("verify")
-        .arg(file.path())
-        .assert()
-        .failure();
-}
+        let file = setup_test_input(dir.path(), FORMATTED_CODE);
 
-#[test]
-fn validate_check_fails_without_input() {
-    let dir = tempdir().unwrap();
+        mdsf_command(dir.path())
+            .arg("verify")
+            .arg(file.path())
+            .assert()
+            .success();
+    }
 
-    mdsf_command(dir.path()).arg("verify").assert().failure();
+    #[test]
+    fn validate_check_command_with_formatted_input_stdin() {
+        let dir = tempdir().unwrap();
+
+        mdsf_command(dir.path()).arg("init").assert().success();
+
+        mdsf_command(dir.path())
+            .arg("verify")
+            .arg("--stdin")
+            .write_stdin(FORMATTED_CODE)
+            .assert()
+            .success();
+    }
+
+    #[test]
+    fn validate_check_command_with_broken_input() {
+        let dir = tempdir().unwrap();
+
+        mdsf_command(dir.path()).arg("init").assert().success();
+
+        let file = setup_test_input(dir.path(), BROKEN_CODE);
+
+        mdsf_command(dir.path())
+            .arg("verify")
+            .arg(file.path())
+            .assert()
+            .failure();
+    }
+
+    #[test]
+    fn validate_check_command_fails_without_input() {
+        let dir = tempdir().unwrap();
+
+        mdsf_command(dir.path()).arg("verify").assert().failure();
+    }
 }
