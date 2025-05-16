7349fa1270f6cc24bd498e6a7b0b60bb95665e97
Mads Hougesen <mads@mhouge.dk>
2025-05-16 22:39:29 +0200

    feat(tools): support pyrefly
    Adds support for the new Python typechecker Pyrefly https://github.com/facebook/pyrefly

---
+++ mdsf/src/tools/pyrefly.rs
@@ -0,0 +1,23 @@
+//!
+//! THIS FILE IS GENERATED USING CODE - DO NOT EDIT MANUALLY
+//!
+
+use crate::runners::CommandType;
+
+#[inline]
+pub fn set_args(
+    mut cmd: std::process::Command,
+    file_path: &std::path::Path,
+) -> std::process::Command {
+    cmd.arg("check");
+    cmd.arg(file_path);
+    cmd
+}
+
+pub const COMMANDS: [CommandType; 3] = [
+    CommandType::Direct("pyrefly"),
+    CommandType::Uv("pyrefly", "pyrefly"),
+    CommandType::Pipx("pyrefly"),
+];
+
+pub const IS_STDIN: bool = false;
