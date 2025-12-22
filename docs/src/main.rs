748e9f0402ce12b8b0092f47c83bba5c62808ed6
Mads Hougesen <mads@mhouge.dk>
2025-12-22 17:10:04 +0100

    feat: support searching in tools

--- docs/src/main.rs
+++ docs/src/main.rs
@@ -1,25 +1,39 @@
 mod error;
-mod packages;
+mod tools;
 
 #[derive(serde::Deserialize, Debug)]
-pub struct PluginFileCommand {
+pub struct ToolFileCommand {
+    #[serde(default)]
+    pub homepage: String,
+
+    #[serde(default)]
+    pub description: String,
+
+    #[serde(default)]
+    pub stdin: bool,
+
     pub arguments: Vec<String>,
 }
 
 #[derive(serde::Deserialize, Debug)]
-pub struct PluginFile {
+pub struct ToolFile {
     pub binary: String,
 
     pub description: String,
 
     pub languages: Vec<String>,
 
+    pub homepage: String,
+
     pub categories: Vec<String>,
 
-    pub commands: std::collections::BTreeMap<String, PluginFileCommand>,
+    pub commands: std::collections::BTreeMap<String, ToolFileCommand>,
+
+    #[serde(default)]
+    pub name: String,
 }
 
-fn find_plugins() -> Result<Vec<PluginFile>, crate::error::Error> {
+fn find_tools() -> Result<Vec<ToolFile>, crate::error::Error> {
     let mut files = Vec::new();
 
     for entry in std::fs::read_dir("tools")? {
@@ -30,9 +44,11 @@ fn find_plugins() -> Result<Vec<PluginFile>, crate::error::Error> {
         if path.is_dir() {
             let f = std::fs::read_to_string(path.join("plugin.json"))?;
 
-            files.push(serde_json::from_str(&f)?);
+            files.push(serde_json::from_str::<ToolFile>(&f)?);
         }
     }
+
+    files.sort_unstable_by(|a, b| a.binary.cmp(&b.binary));
 
     Ok(files)
 }
@@ -49,13 +65,30 @@ fn main() -> Result<(), crate::error::Error> {
     let _ = std::fs::remove_dir_all(path);
     let _ = std::fs::create_dir_all(path);
 
-    let plugins = find_plugins()?;
+    let tools = find_tools()?;
 
-    let _ = std::fs::create_dir_all(path.join("packages"));
+    let tools_dir = path.join("tools");
+
+    let _ = std::fs::create_dir_all(&tools_dir);
     std::fs::write(
-        path.join("packages/index.html"),
-        packages::generate_package_list(&parser, &plugins)?,
+        tools_dir.join("index.html"),
+        tools::generate_tool_list(&parser, &tools)?,
     )?;
+
+    for tool in &tools {
+        let tool_dir = tools_dir.join(if tool.name.is_empty() {
+            tool.binary.clone()
+        } else {
+            tool.name.clone()
+        });
+
+        let _ = std::fs::create_dir_all(&tool_dir);
+
+        std::fs::write(
+            tool_dir.join("index.html"),
+            tools::generate_tool_site(&parser, tool)?,
+        )?;
+    }
 
     Ok(())
 }
